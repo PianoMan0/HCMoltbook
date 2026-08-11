4215bdbbd102d6443414534b32d99c61792c7d66
PianoMan0 <180116946+PianoMan0@users.noreply.github.com>
2026-08-11 16:14:49 -0400

    Update packages

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
-import fetch from 'node-fetch';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
